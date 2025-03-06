d5791560747c0658941c76037299156ff78666a8
Aluno <aluno@IPIRANGA.FATEC>
2025-03-06 16:40:32 -0300

    feat: Implementando Livro de Notas

---
+++ TesteLivroDeNotas.java
@@ -0,0 +1,8 @@
+public class TesteLivroDeNotas{
+    public static void main(String... args){
+        //1. construir um livro de notas
+        LivroDeNotas livroDeNotas = new LivroDeNotas();
+        //2. acionar comportamento de exibir mensagem de boas vindas
+        livroDeNotas.exibirMensagem("POO");
+    }
+}
