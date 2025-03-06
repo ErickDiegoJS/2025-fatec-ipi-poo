dbdb65425d1342d2c1cc0dc7fbfad938e4527ea0
Aluno <aluno@IPIRANGA.FATEC>
2025-03-06 17:50:23 -0300

    fix: teste do livro de notas

--- OO_part1/LivroDeNotas.java
+++ OO_part1/LivroDeNotas.java
@@ -1,5 +1,12 @@
 class LivroDeNotas{
-    void exibirMensagem(String nomeDaDisciplina){
+
+    private String nomeDaDisciplina = null;
+
+    void setNomedaDisciplina(String nomeDaDisciplina){
+        this.nomeDaDisciplina = nomeDaDisciplina;
+    }
+
+    void exibirMensagem(){
         System.out.println("Bem Vindo ao Livro de Notas de " + nomeDaDisciplina);
     }
 }
--- OO_part1/TesteLivroDeNotas.java
+++ OO_part1/TesteLivroDeNotas.java
@@ -2,7 +2,15 @@ public class TesteLivroDeNotas{
     public static void main(String... args){
         //1. construir um livro de notas
         LivroDeNotas livroDeNotas = new LivroDeNotas();
+        //livroDeNotas.nomeDaDisciplina = "POO";
+        livroDeNotas.setNomedaDisciplina("POO");
+
+        LivroDeNotas livro2 = new LivroDeNotas();
+        //livro2.nomeDaDisciplina = "BD";
+        livro2.setNomedaDisciplina("BD");
+
         //2. acionar comportamento de exibir mensagem de boas vindas
-        livroDeNotas.exibirMensagem("POO");
+        livroDeNotas.exibirMensagem();
+        livro2.exibirMensagem();
     }
 }
