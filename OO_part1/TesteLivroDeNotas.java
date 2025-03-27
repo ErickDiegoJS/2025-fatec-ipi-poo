835fe188cab877dbe6f42f106f87cdbbab48a5d2
LeviNobody <134021281+ErickDiegoJS@users.noreply.github.com>
2025-03-27 17:02:07 -0300

    Atualizar o TesteLivroDeNotas.java

--- OO_part1/TesteLivroDeNotas.java
+++ OO_part1/TesteLivroDeNotas.java
@@ -1,17 +1,17 @@
-public class TesteLivroDeNotas{
-    public static void main(String... args){
-        //1. construir um livro de notas
+public class TesteLivroDeNotas {
+    public static void main(String... args) {
+        // 1. construir um livro de notas
         LivroDeNotas livroDeNotas = new LivroDeNotas("POO");
         //livroDeNotas.nomeDaDisciplina = "POO";
         //jaclivroDeNotas.setNomedaDisciplina("POO");
 
-        System.out.println(livroDeNotas.getnomeDaDisciplina());
+        System.out.println(livroDeNotas.getNomeDaDisciplina());
 
         //LivroDeNotas livro2 = new LivroDeNotas();
         //livro2.nomeDaDisciplina = "BD";
         //livro2.setNomedaDisciplina("BD");
 
-        //2. acionar comportamento de exibir mensagem de boas vindas
+        // 2. acionar comportamento de exibir mensagem de boas vindas
         livroDeNotas.exibirMensagem();
         //livro2.exibirMensagem();
     }
