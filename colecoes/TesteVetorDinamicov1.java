75a883508ba7df304cc5f58b46ad250e775a5883
LeviNobody <134021281+ErickDiegoJS@users.noreply.github.com>
2025-04-10 18:19:04 -0300

    Update TesteVetorDinamicov1.java

--- colecoes/TesteVetorDinamicov1.java
+++ colecoes/TesteVetorDinamicov1.java
@@ -1,10 +1,11 @@
 package colecoes;
 import java.util.Random;
-public class TesteVetorDinamicoV1 {
+public class TesteVetorDinamicov1 {
   public static void main(String[] args) throws Exception {
     var gerador = new Random();
     var v = new VetorDinamico();
     while(true){
+      //o metodo possui um intervalo de 0 a 1 automatico
       var oQueFazer = gerador.nextDouble();
       if(oQueFazer <= 0.5){
         var numero = gerador.nextInt(1, 7);//[1, 7)
